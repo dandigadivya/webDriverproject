40ee6c801d629474170720bad9f83a7deb7a23a5
dandi <dandi@Divya>
2024-04-12 16:06:46 +0530

    New class with method Added

---
+++ src/main/java/april/Demo.java
@@ -0,0 +1,27 @@
+package april;
+
+public class Demo {
+public void add() {
+	int a=34,b=65,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void div() {
+	int a=654,b=124,c;
+	c=a/b;
+	System.out.println(c);
+}
+
+public void mul() {
+	int a=34,b=65,c;
+	c=a*b;
+	System.out.println(c);
+}
+
+public void sub() {
+	int a=345,b=65,c;
+	c=a-b;
+	System.out.println(c);
+}
+
+}
